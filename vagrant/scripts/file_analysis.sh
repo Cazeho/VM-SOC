1f8499523d354b46cd8a6fb1934edfe15914c3af
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-03-08 11:58:51 +0100

    Update file_analysis.sh

--- vagrant/scripts/file_analysis.sh
+++ vagrant/scripts/file_analysis.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
 pip install oletools
 pip2 install -U balbuzard
 pip install LnkParse3
+
+
+
+
+cd /opt
+git clone https://github.com/jesparza/peepdf
+cd peepdf
+sed -i 's/env python$/env python2/' peepdf.py
+ln -s $PWD/peepdf.py /usr/local/bin/peepdf
