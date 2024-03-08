ba2085d8621625379242867a31ed54474e8c17cc
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-03-08 12:02:58 +0100

    Update velociraptor.sh

--- vagrant/scripts/velociraptor.sh
+++ vagrant/scripts/velociraptor.sh
@@ -2,6 +2,6 @@
 
 cd /opt
 
-wget https://github.com/Velocidex/velociraptor/releases/download/v0.6.0-rc1/velociraptor-v0.6.0-rc1-linux-amd64
-chmod +x velociraptor-v0.6.0-rc1-linux-amd64
-mv velociraptor-v0.6.0-rc1-linux-amd64 /usr/local/bin/velociraptor
+wget https://github.com/Velocidex/velociraptor/releases/download/v0.7.1/velociraptor-v0.7.1-1-linux-amd64
+chmod +x velociraptor-v0.7.1-1-linux-amd64
+mv velociraptor-v0.7.1-1-linux-amd64 /usr/local/bin/velociraptor
