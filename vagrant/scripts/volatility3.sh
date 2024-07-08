bc38b9d2596073a01d769b99863889d1cdc1ec19
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-07-08 10:59:22 +0200

    Update and rename volatility3 to volatility3.sh

--- vagrant/scripts/volatility3.sh
+++ vagrant/scripts/volatility3.sh
@@ -3,3 +3,4 @@ git clone https://github.com/volatilityfoundation/volatility3.git
 cd volatility3
 pip install -r requirements.txt
 pip install pyOpenSSL==20.0.1
+ln -s $PWD/vol.py /usr/local/bin/volatility3
