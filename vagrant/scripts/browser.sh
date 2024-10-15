5a640820a29037c00bd001ac8de0b7c003c0c929
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-10-15 19:48:06 +0200

    Create browser.sh

---
+++ vagrant/scripts/browser.sh
@@ -0,0 +1,11 @@
+version: "2.1"
+services:
+  kasm:
+    image: lscr.io/linuxserver/kasm:latest
+    container_name: kasm
+    privileged: true
+    environment:
+      - KASM_PORT=8443
+    ports:
+      - 3000:3000
+      - 8443:443
