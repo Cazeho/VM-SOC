c3318bc1adf18cedea4a0956fa3f927cd527229c
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-10-19 19:33:10 +0200

    Update hayabusa.sh

--- vagrant/scripts/hayabusa.sh
+++ vagrant/scripts/hayabusa.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup update stable
+
+
+git clone https://github.com/Yamato-Security/hayabusa.git --recursive
+cd hayabusa && cargo build --release
+ln -s $PWD/target/release/hayabusa /usr/local/bin/hayabusa
+hayabusa update-rules
 
